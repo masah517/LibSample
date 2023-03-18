580dceda6244e439a8e3802b045327fd5c98b9ce
masah517 <masatech517@gmail.com>
2023-03-18 18:48:26 +0900

    depencyとKotlinのバージョンの修正

---
+++ image-preview/src/main/java/com/masa/image_preview/ImagePreview.kt
@@ -0,0 +1,9 @@
+package com.masa.image_preview
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ImagePreview() {
+    Text(text = "My Rest")
+}
